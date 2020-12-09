dd82b08a97432a3422cad5c1674d9b05cbfc45e4
Sofia <sofia.fredmans@gmail.com>
2020-12-09 21:50:24 +0100

    cart & cart item UI finished

--- src/components/navbar/styles.js
+++ src/components/navbar/styles.js
@@ -6,10 +6,7 @@ export default makeStyles((theme) => ({
   appBar: {
     boxShadow: 'none',
     borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
-    [theme.breakpoints.up('sm')]: {
-      width: `calc(100% - ${drawerWidth}px)`,
-      marginLeft: drawerWidth,
-    },
+
   },
   title: {
     flexGrow: 1,
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -4,23 +4,16 @@ import Grid from '@material-ui/core/Grid';
 import Product from './product/Product';
 import useStyles from './styles'; 
 
-
-const products = [
-    { id: 1, name: "Shoes", description: "Running schoes", price:"$10" },
-    { id: 2, name: 'Bike', description: "Super fast bike", price:"$5" },
-];
-
-
-
-const Products = () => {
+const Products = ({products, onAddToCart}) => {
     const classes = useStyles();
+
     return(
         <main className={classes.content}>
             <div className={classes.toolbar}/>
         <Grid container justify="center" spacing={4}>
             {products.map((product) => (
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                    <Product product={product} />
+                    <Product product={product} onAddToCart={onAddToCart} />
                 </Grid>
             ))}
         </Grid>
